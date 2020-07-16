41418550e492dd498e2277386aceed0e162a3738
irenevbl <66783543+irenevbl@users.noreply.github.com>
2020-07-16 09:02:28 +0200

    Add files via upload

---
+++ cell_type_composition/celltypedifferences_elife_25072020.R
@@ -0,0 +1,32 @@
+########## Multiple testing correction for cell type composition differences for the elife method #############
+####################################################################################
+# Elife method obtained externally from https://elifesciences.org/articles/43882   #
+####################################################################################
+
+####################################################################################
+# libraries                                                                        #
+####################################################################################
+library(Seurat)
+library(ggplot2)
+
+####################################################################################
+# Perform post-hoc Benjamin-Hochberg correction on elife p-values with W=0.1      #
+####################################################################################
+# Perform post-hoc Benjamin-Hochberg correction on elife p-values with W=0.1 #
+allbaselinet24h <- read.table("/groups/umcg-wijmenga/tmp04/projects/1M_cells_scRNAseq/ongoing/Cardiology/cell_type_composition/elife_2020/all_baseline_t24h_20200710.tsv")
+allbaselinet24h <- allbaselinet24h[c("0.1","0.05"), ]
+for(colname in colnames(allbaselinet24h)){
+  allbaselinet24h[[paste(colname,"corrected", sep=".")]] <- p.adjust(allbaselinet24h[[colname]], n=6, method = c("hochberg"))
+}
+
+tables <- list.files("/groups/umcg-wijmenga/tmp04/projects/1M_cells_scRNAseq/ongoing/Cardiology/cell_type_composition/elife_2020/", include.dirs = F)
+for(table in tables){
+  try({
+    elife <- read.table(paste("/groups/umcg-wijmenga/tmp04/projects/1M_cells_scRNAseq/ongoing/Cardiology/cell_type_composition/elife_2020/", table, sep=""), header=T, sep='\t')
+    elife <- elife[c("0.1","0.05"), ]
+    for(colname in colnames(elife)){
+      elife[[paste(colname,"corrected", sep=".")]] <- p.adjust(elife[[colname]], n=6, method = c("hochberg"))
+    }
+    write.table(elife, paste("/home/umcg-ivanblokland/stemi/cell_type_composition/", table, "cor.tsv", sep=''), sep='\t')
+  })
+}
