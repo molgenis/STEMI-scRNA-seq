1a722a4900345882a7f554bbb733efd4d363fba9
Roy Oelen <r.oelen@umcg.nl>
2021-01-27 18:12:40 +0100

    - added pairwise DE option to limma dream - added pathway plot option where it is now possible to average the expression per participant

--- differential_expression/stemi_limma_dream.R
+++ differential_expression/stemi_limma_dream.R
@@ -62,6 +62,61 @@ dreamer <- function(seurat_object, output_loc, aggregates=c('assignment.final', 
   saveRDS(vars, output_loc)
 }
 
+dream_pairwise <- function(seurat_object, output_loc, condition_combinations, aggregates=c('assignment.final', 'timepoint.final')){
+    # grab the countmatrix
+    countMatrix <- seurat_object@assays$RNA@counts
+    # get the metadata
+    metadata <- seurat_object@meta.data
+    # create the groups to aggregate on, here it's on sample and timepoint
+    groups <- metadata[, aggregates]
+    # create an aggregated counts matrix
+    aggregate_countMatrix <- t(aggregate.Matrix(t(countMatrix), groupings = groups, fun = 'sum'))
+    # create aggregated metadata
+    aggregate_metadata <- unique(metadata[, aggregates])
+    # set the rownames to be the same as the countsMatrix aggregate colnames, should go correctly if data is valid
+    rownames(aggregate_metadata) <- colnames(aggregate_countMatrix)
+    
+    # filter genes by number of counts
+    isexpr = rowSums(cpm(aggregate_countMatrix)>0.1) >= 5
+    
+    # Standard usage of limma/voom
+    geneExpr = DGEList( aggregate_countMatrix[isexpr,] )
+    geneExpr = calcNormFactors( geneExpr )
+    
+    # Specify parallel processing parameters
+    # this is used implicitly by dream() to run in parallel
+    param = SnowParam(4, "SOCK", progressbar=TRUE)
+    register(param)
+    
+    # The variable to be tested must be a fixed effect
+    form <- ~ 0 + timepoint.final + (1|assignment.final) 
+    
+    # estimate weights using linear mixed model of dream
+    vobjDream = voomWithDreamWeights( geneExpr, form, aggregate_metadata )
+    
+    # do each combination
+    for(combination_name in names(condition_combinations)){
+      # grab the combination
+      combination <- condition_combinations[[combination_name]]
+      
+      # define and then cbind contrasts
+      L = getContrast( vobjDream, form, aggregate_metadata, c(combination[1], combination[2]))
+      
+      # fit contrast
+      fit = dream( vobjDream, form, metadata_metadata, L)
+      
+      # grab the exact fit
+      limma_result <- topTable(fit, coef=c('L1'), number=length(fit$F.p.value))
+      
+      # set an output location
+      limma_output_loc <- paste(output_loc, combination_name, '.tsv', sep = '')
+      # write the result
+      write.table(limma_result, limma_output_loc, sep = '\t', row.names = T)
+    }
+    
+}
+
+
 
 # read command line arguments
 args = commandArgs(trailingOnly=TRUE)
@@ -69,7 +124,7 @@ args = commandArgs(trailingOnly=TRUE)
 orig_ident <- args[1]
 cell_type <- args[2]
 # read the file
-cardio.integrated <- readRDS('/groups/umcg-wijmenga/tmp01/projects/1M_cells_scRNAseq/ongoing/Cardiology/objects/cardio.integrated_20201102.rds')
+cardio.integrated <- readRDS('/groups/umcg-wijmenga/tmp01/projects/1M_cells_scRNAseq/ongoing/Cardiology/objects/cardio.integrated.20201209.rds')
 # subset to the cell type and original identity
 ct_and_oi <- cardio.integrated[, (cardio.integrated@meta.data$cell_type_lowerres == cell_type & cardio.integrated@meta.data$orig.ident == orig_ident)]
 # clear some memory
@@ -79,3 +134,25 @@ output_limma_base <- '/groups/umcg-wijmenga/tmp01/projects/1M_cells_scRNAseq/ong
 output_limma <- paste(output_limma_base, orig_ident, '_', cell_type, '_pseudobulked.rds', sep='')
 # call the method
 dreamer(ct_and_oi, output_limma)
+
+
+# make a list of condition combinations to test 1 vs 1
+condition_combinations <- list()
+ut_baseline <- c('UT', 'Baseline')
+ut_t24h <- c('UT', 't24h')
+ut_t8w <- c('UT', 't8w')
+baseline_t24h <- c('Baseline', 't24h')
+baseline_t8w <- c('Baseline', 't8w')
+t24ht8w <- c('t24h', 't8w')
+# add these to the list
+condition_combinations[['ut_baseline']] <- ut_baseline
+condition_combinations[['ut_t24h']] <- ut_t24h
+condition_combinations[['ut_t8w']] <- ut_t8w
+condition_combinations[['baseline_t24h']] <- baseline_t24h
+condition_combinations[['baseline_t8w']] <- baseline_t8w
+condition_combinations[['t24ht8w']] <- t24ht8w
+# set the base path
+limma_out_base_path <- '/groups/umcg-wijmenga/tmp01/projects/1M_cells_scRNAseq/ongoing/Cardiology/differential_expression/limma/output/dream_contrast_'
+limma_out_ct_path <- paste(limma_out_base_path, cell_type, sep='')
+# do the analysis
+dream_pairwise(ct_and_oi, limma_out_ct_path, condition_combinations)
--- differential_expression/stemi_plot_pathways.R
+++ differential_expression/stemi_plot_pathways.R
@@ -11,7 +11,7 @@ library(ggplot2)
 ####################
 
 # plot the module score in a boxplot
-plot_average_expression <- function(seurat_object, module_score_column_name, cell_types=c('B', 'CD4T', 'CD8T', 'DC', 'monocyte', 'NK'), conditions=c('UT', 'Baseline', 't24h', 't8w'), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', title='pathway', color_by_ct=T){
+plot_average_expression <- function(seurat_object, module_score_column_name, cell_types=c('B', 'CD4T', 'CD8T', 'DC', 'monocyte', 'NK'), conditions=c('UT', 'Baseline', 't24h', 't8w'), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', title='pathway', color_by_ct=T, to_per_part=F, participant_column='assignment.final'){
   # get the metadata
   metadata <- seurat_object@meta.data
   # limit the cell types
@@ -22,8 +22,11 @@ plot_average_expression <- function(seurat_object, module_score_column_name, cel
   metadata$ct_column <- metadata[[cell_type_column]]
   metadata$tp_column <- metadata[[condition_column]]
   metadata$msc_column <- metadata[[module_score_column_name]]
-  print(head(metadata))
   metadata$cttp_column <- paste(metadata$ct_column, metadata$tp_column, sep = '\n')
+  # convert to per_participant if requested
+  if(to_per_part){
+    metadata <- metadata_to_average_module_per_part(metadata, cttp_column='cttp_column', msc_column='msc_column', ct_column='ct_column', condition_column='tp_column', participant_column=participant_column)
+  }
   # grab our dict for colors
   cc <- get_color_coding_dict()
   # if the colouring is by cell type
@@ -51,8 +54,36 @@ plot_average_expression <- function(seurat_object, module_score_column_name, cel
   }
 }
 
+metadata_to_average_module_per_part <- function(metadata, cttp_column='cttp_column', msc_column='msc_column', ct_column='ct_column', condition_column='tp_column', participant_column='assignment.final'){
+  metadata_averaged <- NULL
+  for(cttp in unique(metadata[[cttp_column]])){
+    # subset to cell type and condition combo
+    metadata_cttp <- metadata[metadata[[cttp_column]] == cttp ,]
+    # check for each participant here
+    for(participant in unique(metadata_cttp[[participant_column]])){
+      # calculate the mean
+      mean_participant <- mean(metadata_cttp[metadata_cttp[[participant_column]] == participant, msc_column])
+      # get other data
+      ct <- as.character(metadata_cttp[metadata_cttp[[participant_column]] == participant, ct_column][1])
+      tp <- as.character(metadata_cttp[metadata_cttp[[participant_column]] == participant, condition_column][1])
+      # set as a dataframe
+      metadata_averaged_row <- data.frame(cttp_column=c(cttp), msc_column=c(mean_participant), ct_column=c(ct), condition_column=c(tp), participant_column=c(participant), stringsAsFactors = F)
+      # set as initial or append
+      if(is.null(metadata_averaged)){
+        metadata_averaged <- metadata_averaged_row
+      }
+      else{
+        metadata_averaged <- rbind(metadata_averaged, metadata_averaged_row)
+      }
+    }
+  }
+  # set colnames as before
+  colnames(metadata_averaged) <- c(cttp_column, msc_column, ct_column, condition_column, participant_column)
+  return(metadata_averaged)
+}
+
 # add the module score from a table of genes (now using as pathway plot)
-add_module_score_from_table <- function(pathway_gene_table_loc, pathway_name, seurat_object, and_plot=T, cell_types=c('B', 'CD4T', 'CD8T', 'DC', 'monocyte', 'NK'), conditions=c('UT', 'Baseline', 't24h', 't8w'), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = T){
+add_module_score_from_table <- function(pathway_gene_table_loc, pathway_name, seurat_object, and_plot=T, cell_types=c('B', 'CD4T', 'CD8T', 'DC', 'monocyte', 'NK'), conditions=c('UT', 'Baseline', 't24h', 't8w'), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = T, to_per_part=F, participant_column='assignment.final'){
   # get the cytokine genes
   pathway_df <- read.table(pathway_gene_table_loc)
   pathway_genes <- pathway_df$V1
@@ -63,7 +94,7 @@ add_module_score_from_table <- function(pathway_gene_table_loc, pathway_name, se
   # add the module score
   seurat_object <- AddModuleScore(seurat_object, features = pathway_list_seurat_object, name = pathway_name)
   if(and_plot){
-    plot_average_expression(seurat_object, module_score_column_name=paste(pathway_name, '1', sep = ''), cell_types=cell_types, conditions=conditions, cell_type_column=cell_type_column, condition_column=condition_column, title=pathway_name, color_by_ct = color_by_ct)
+    plot_average_expression(seurat_object, module_score_column_name=paste(pathway_name, '1', sep = ''), cell_types=cell_types, conditions=conditions, cell_type_column=cell_type_column, condition_column=condition_column, title=pathway_name, color_by_ct = color_by_ct, to_per_part=to_per_part, participant_column=participant_column)
   }
   return(seurat_object)
 }
@@ -99,7 +130,7 @@ get_color_coding_dict <- function(){
 ####################
 
 # set location of object
-cardio.integrated.loc <- '/groups/umcg-wijmenga/tmp04/projects/1M_cells_scRNAseq/ongoing/Cardiology/objects/cardio.integrated_20201209.rds'
+cardio.integrated.loc <- '/groups/umcg-wijmenga/tmp04/projects/1M_cells_scRNAseq/ongoing/Cardiology/objects/cardio.integrated.20201209.rds'
 cardio.integrated <- readRDS(cardio.integrated.loc)
 
 # location of the pathway genes
@@ -113,6 +144,17 @@ plot_output_loc <- '/groups/umcg-wijmenga/tmp04/projects/1M_cells_scRNAseq/ongoi
 cardio.v2 <- subset(cardio.integrated, subset = chem == 'V2')
 cardio.v3 <- subset(cardio.integrated, subset = chem == 'V3')
 
+# normalize RNA
+DefaultAssay(cardio.v2) <- 'RNA'
+cardio.v2 <- NormalizeData(cardio.v2)
+DefaultAssay(cardio.v3) <- 'RNA'
+cardio.v3 <- NormalizeData(cardio.v3)
+# normalize SCT
+DefaultAssay(cardio.v2) <- 'SCT'
+cardio.v2 <- SCTransform(cardio.v2)
+DefaultAssay(cardio.v3) <- 'SCT'
+cardio.v3 <- SCTransform(cardio.v3)
+
 # plot each pathway
 for(pathway_file in pathway_files){
   # set the full file loc of the gene list
@@ -120,17 +162,32 @@ for(pathway_file in pathway_files){
   # create a regex to get the last index of .
   last_dot_pos <- "\\.[^\\.]*$"
   # this allows us to remove the filename extention, which we will use as the name
-  pathway_name <- substr(full_pathway_file, 1, regexpr(last_dot_pos, pathway_file)-1)
+  pathway_name <- substr(pathway_file, 1, regexpr(last_dot_pos, pathway_file)-1)
   # we need to replace dashes with underscores
   pathway_name <- gsub('-', '_', pathway_name)
-  # do for v2
-  add_module_score_from_table(full_pathway_file, pathway_name, cardio.v2, cell_types=c('monocyte'), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = F)
-  output_loc_v2 <- paste(plot_output_loc, 'mono_v2_', pathway_name, '.png', sep = '')
-  ggsave(output_loc_v2, width = 10, height = 10)
-  # and for v3
-  add_module_score_from_table(full_pathway_file, pathway_name, cardio.v3, cell_types=c('monocyte'), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = F)
-  output_loc_v2 <- paste(plot_output_loc, 'mono_v3_', pathway_name, '.png', sep = '')
-  ggsave(output_loc_v2, width = 10, height = 10)
+  # do for each cell type
+  for(cell_type in unique(cardio.integrated@meta.data$cell_type_lowerres)){
+    # do for v2
+    DefaultAssay(cardio.v2) <- 'RNA'
+    add_module_score_from_table(full_pathway_file, pathway_name, cardio.v2, cell_types=c(cell_type), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = F, to_per_part=T, participant_column='assignment.final')
+    output_loc_v2 <- paste(plot_output_loc, cell_type, '_v2_', pathway_name, '_RNA_perpart.png', sep = '')
+    ggsave(output_loc_v2, width = 10, height = 10)
+    # do for v2
+    DefaultAssay(cardio.v2) <- 'SCT'
+    add_module_score_from_table(full_pathway_file, pathway_name, cardio.v2, cell_types=c(cell_type), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = F, to_per_part=T, participant_column='assignment.final')
+    output_loc_v2 <- paste(plot_output_loc, cell_type, '_v2_', pathway_name, '_SCT_perpart.png', sep = '')
+    ggsave(output_loc_v2, width = 10, height = 10)
+    # and for v3
+    DefaultAssay(cardio.v3) <- 'RNA'
+    add_module_score_from_table(full_pathway_file, pathway_name, cardio.v3, cell_types=c(cell_type), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = F, to_per_part=T, participant_column='assignment.final')
+    output_loc_v2 <- paste(plot_output_loc, cell_type, '_v3_', pathway_name, '_RNA_perpart.png', sep = '')
+    ggsave(output_loc_v2, width = 10, height = 10)
+    # and for v3
+    DefaultAssay(cardio.v3) <- 'SCT'
+    add_module_score_from_table(full_pathway_file, pathway_name, cardio.v3, cell_types=c(cell_type), cell_type_column='cell_type_lowerres', condition_column='timepoint.final', color_by_ct = F, to_per_part=T, participant_column='assignment.final')
+    output_loc_v2 <- paste(plot_output_loc, cell_type, '_v3_', pathway_name, '_SCT_perpart.png', sep = '')
+    ggsave(output_loc_v2, width = 10, height = 10)
+  }
 }
 
 
