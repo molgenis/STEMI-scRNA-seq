25c46444dc39a053a745629477750817e35b3562
Roy Oelen <r.oelen@umcg.nl>
2024-02-18 22:45:57 +0100

    plotting cell types for pc30 vs pc50

--- preprocessing/stemi_compare_clustering_pcs.R
+++ preprocessing/stemi_compare_clustering_pcs.R
@@ -73,6 +73,41 @@ create_confusion_matrix <- function(assignment_table, truth_column, prediction_c
 }
 
 
+# add metadata that is based on existing incomplete metadata in the seurat object
+add_imputed_meta_data <- function(seurat_object, column_to_transform, column_to_reference, column_to_create){
+  # add the column
+  seurat_object@meta.data[[column_to_create]] <- NA
+  # go through the grouping we have for the entire object
+  for(group in unique(seurat_object@meta.data[!is.na(seurat_object@meta.data[[column_to_transform]]), column_to_transform])){
+    # subset to get only this group
+    seurat_group <- seurat_object[, !is.na(seurat_object@meta.data[[column_to_transform]]) & seurat_object@meta.data[[column_to_transform]] == group]
+    best_group <- 'unknown'
+    best_number <- 0
+    # check against the reference column
+    for(reference in unique(seurat_group@meta.data[[column_to_reference]])){
+      # we don't care for the NA reference, if we had all data, we wouldn't need to do this anyway
+      if(is.na(reference) == F){
+        # grab the number of cells in this group, with this reference
+        number_of_reference_in_group <- nrow(seurat_group@meta.data[!(is.na(seurat_group@meta.data[[column_to_reference]])) & seurat_group@meta.data[[column_to_reference]] == reference,])
+        correctpercent <- number_of_reference_in_group/ncol(seurat_group)
+        print(paste(group,"matches", reference, correctpercent,sep=" "))
+        # update numbers if better match
+        if(number_of_reference_in_group > best_number){
+          best_number <- number_of_reference_in_group
+          best_group <- reference
+        }
+      }
+    }
+    print(paste("setting ident:",best_group,"for group", group, sep=" "))
+    # set this best identity
+    seurat_object@meta.data[!is.na(seurat_object@meta.data[[column_to_transform]]) & seurat_object@meta.data[[column_to_transform]] == group, column_to_create] <- best_group
+    # force cleanup
+    rm(seurat_group)
+  }
+  return(seurat_object)
+}
+
+
 
 ####################
 # Main Code        #
@@ -88,7 +123,7 @@ options(future.globals.maxSize = 2000 * 1000 * 1024^2)
 set.seed(7777)
 
 # location of the objects
-cardio_integrated_loc <- '/groups/umcg-franke-scrna/tmp01/releases/blokland-2020/v1/seurat/cardio.integrated.20210301.rds'
+cardio_integrated_loc <- '/groups/umcg-franke-scrna/tmp03/releases/blokland-2020/v1/seurat/cardio.integrated.20210301.rds'
 
 # read the object
 cardio_integrated <- readRDS(cardio_integrated_loc)
@@ -122,3 +157,62 @@ create_confusion_matrix(
     pc30=cardio_integrated@meta.data[['seurat_clusters']], 
     pc50=cardio_integrated_pc50@meta.data[['seurat_clusters']]), 
   'pc30', 'pc50', 'leiden clusters 30 PCs', 'leiden clusters 50 PCs')
+
+# do the same thing we did before
+cardio_integrated@meta.data$cell_type <- cardio_integrated@meta.data$predicted.celltype.l2
+cardio_integrated@meta.data$cell_type <- as.character(cardio_integrated@meta.data$cell_type)
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'NK', ]$cell_type <- 'NKdim'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'NK_CD56bright', ]$cell_type <- 'NKbright'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'CD14 Mono', ]$cell_type <- 'cMono'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'CD16 Mono', ]$cell_type <- 'ncMono'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'Plasmablast', ]$cell_type <- 'plasmablast'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'Platelet', ]$cell_type <- 'platelet'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'Eryth', ]$cell_type <- 'eryth'
+cardio_integrated@meta.data[cardio_integrated@meta.data$cell_type == 'Doublet', ]$cell_type <- 'doublet'
+cardio_integrated@meta.data$cell_type <- gsub(' ', '_', cardio_integrated@meta.data$cell_type)
+cardio_integrated <- add_imputed_meta_data(cardio_integrated, 'seurat_clusters', 'cell_type', 'ct_clus_prop')
+cd4t <- c('Treg', 'CD4_Naive', 'CD4_TCM', 'CD4_TEM', 'CD4_CTL', 'CD4_Proliferating')
+cd8t <- c('MAIT', 'CD8_Naive', 'CD8_TCM', 'CD8_TEM', 'CD8_Proliferating')
+t_other <- c('dnT', 'gdT', 'ILC')
+nk <- c('NKdim', 'NKbright', 'NK_Proliferating')
+monocyte <- c('cMono', 'ncMono')
+dc <- c('cDC1', 'cDC2', 'pDC', 'ASDC')
+b <- c('B_naive', 'B_intermediate', 'B_memory')
+cardio_integrated@meta.data$cell_type_lowerres <- cardio_integrated@meta.data$ct_clus_prop
+cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% cd4t, ]$cell_type_lowerres <- 'CD4T'
+cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% cd8t, ]$cell_type_lowerres <- 'CD8T'
+#cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% t_other, ]$cell_type_lowerres <- 'T_other'
+cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% nk, ]$cell_type_lowerres <- 'NK'
+cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% monocyte, ]$cell_type_lowerres <- 'monocyte'
+cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% dc, ]$cell_type_lowerres <- 'DC'
+cardio_integrated@meta.data[cardio_integrated@meta.data$ct_clus_prop %in% b, ]$cell_type_lowerres <- 'B'
+
+# do the same thing we did before
+cardio_integrated_pc50@meta.data$cell_type <- cardio_integrated_pc50@meta.data$predicted.celltype.l2
+cardio_integrated_pc50@meta.data$cell_type <- as.character(cardio_integrated_pc50@meta.data$cell_type)
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'NK', ]$cell_type <- 'NKdim'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'NK_CD56bright', ]$cell_type <- 'NKbright'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'CD14 Mono', ]$cell_type <- 'cMono'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'CD16 Mono', ]$cell_type <- 'ncMono'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'Plasmablast', ]$cell_type <- 'plasmablast'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'Platelet', ]$cell_type <- 'platelet'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'Eryth', ]$cell_type <- 'eryth'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$cell_type == 'Doublet', ]$cell_type <- 'doublet'
+cardio_integrated_pc50@meta.data$cell_type <- gsub(' ', '_', cardio_integrated_pc50@meta.data$cell_type)
+cardio_integrated_pc50 <- add_imputed_meta_data(cardio_integrated_pc50, 'seurat_clusters', 'cell_type', 'ct_clus_prop')
+cardio_integrated_pc50@meta.data$cell_type_lowerres <- cardio_integrated_pc50@meta.data$ct_clus_prop
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% cd4t, ]$cell_type_lowerres <- 'CD4T'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% cd8t, ]$cell_type_lowerres <- 'CD8T'
+#cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% t_other, ]$cell_type_lowerres <- 'T_other'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% nk, ]$cell_type_lowerres <- 'NK'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% monocyte, ]$cell_type_lowerres <- 'monocyte'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% dc, ]$cell_type_lowerres <- 'DC'
+cardio_integrated_pc50@meta.data[cardio_integrated_pc50@meta.data$ct_clus_prop %in% b, ]$cell_type_lowerres <- 'B'
+
+# now we'll check cluster membership
+create_confusion_matrix(
+  data.frame(
+    pc30=cardio_integrated@meta.data[['cell_type_lowerres']], 
+    pc50=cardio_integrated_pc50@meta.data[['cell_type_lowerres']]), 
+  'pc30', 'pc50', 'cell types 30 PCs', 'cell types 50 PCs')
+# saved as stemi_pc30_vs_pc50_celltypes
